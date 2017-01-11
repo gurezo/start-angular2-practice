216b62ce4e263bb4ee73c5d75c3e8f15756c8d5d
gurezo <akihiko.kigure@gmail.com>
2017-01-11 21:37:41 +0900

    first commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { StartAngular2PracticePage } from './app.po';
+
+describe('start-angular2-practice App', function() {
+  let page: StartAngular2PracticePage;
+
+  beforeEach(() => {
+    page = new StartAngular2PracticePage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
