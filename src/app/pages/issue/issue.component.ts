85d1b3d395952c1e7398b1fb7547314b73944674
gurezo <akihiko.kigure@gmail.com>
2017-01-18 20:49:48 +0900

    Issue tracker ページ作成実装

--- src/app/pages/issue/issue.component.ts
+++ src/app/pages/issue/issue.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Issue } from './issue';
 
 @Component({
   selector: 'app-issue',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssueComponent implements OnInit {
 
+  issue: Issue;
+  issues: Issue[];
+
   constructor() { }
 
   ngOnInit() {
+    this.issue = new Issue;
+    this.issues = [];
+  }
+
+  public onSubmit(forms: NgForm): void {
+    const issue = {
+      title: forms.value.title,
+      desc: forms.value.desc
+    }
+
+    this.issues.push(issue);
+    forms.reset();
+  }
+
+  public onDelete(index: number): void {
+    this.issues.splice(index, 1);
   }
 
 }
--- src/app/pages/issue/issue.module.ts
+++ src/app/pages/issue/issue.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { IssueComponent } from './issue.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
-  declarations: []
+  declarations: [
+    IssueComponent
+  ]
 })
 export class IssueModule { }
