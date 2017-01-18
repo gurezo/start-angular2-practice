e53fc2e5656e1f6e3edbda5598afef05ee25ecdf
gurezo <akihiko.kigure@gmail.com>
2017-01-18 14:59:18 +0900

    子ルータの設定実施

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { IssueModule } from './issue/issue.module';
+import { WikiModule } from './wiki/wiki.module';
+
 import { TopComponent } from './top/top.component';
-import { PagesComponent } from './pages/pages.component';
+import { PagesComponent } from './pages.component';
+
+import { pagesRouting, pagesRoutingProviders } from './page.routes';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    IssueModule,
+    WikiModule,
+    pagesRouting
   ],
   declarations: [TopComponent, PagesComponent]
 })
