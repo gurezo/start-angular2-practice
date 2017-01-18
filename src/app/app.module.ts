55444c8f35f3eb025c2916ad184ffb75ef154ad3
gurezo <akihiko.kigure@gmail.com>
2017-01-18 15:04:19 +0900

    親ルータの設定実施

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { IssueComponent } from './issue/issue.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { WikiComponent } from './wiki/wiki.component';
+import { FooterComponent } from './footer/footer.component';
 
 import { routing, appRoutingProviders } from './app.routes';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    IssueComponent,
     PageNotFoundComponent,
-    WikiComponent,
     FooterComponent
   ],
   imports: [
