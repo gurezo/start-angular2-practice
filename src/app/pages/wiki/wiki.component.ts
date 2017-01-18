7cacb03ac92c8b96e637cb64d8ce9ee83e04cb7a
gurezo <akihiko.kigure@gmail.com>
2017-01-18 17:29:49 +0900

    pipeを使ったwikiページ作成

--- src/app/pages/wiki/wiki.component.ts
+++ src/app/pages/wiki/wiki.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WikiComponent implements OnInit {
 
+  wiki: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.wiki =　localStorage.getItem('amke');
   }
 
 }
