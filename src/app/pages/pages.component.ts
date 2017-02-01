cb74db9edc14742f90d70b72972cfea0ada90e6b
gurezo <akihiko.kigure@gmail.com>
2017-02-01 23:20:09 +0900

    animations ',' expected.エラー未解決

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/core';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styleUrls: ['./pages.component.scss']
+  styleUrls: ['./pages.component.scss'],
+  animations: [
+    trigger('menuState', [
+        state('inactive', styles({
+            transform: 'scale(1)'
+          })),
+        state('active', styles({
+          borderBottom: '2px solid #333333',
+          transform: 'scale(1.2)'
+        })),
+        transition('inactive => active', animate('300ms ease-in')),
+        transition('active => inactive', animate('300ms ease-out'))
+      ])
+    ]
 })
+
 export class PagesComponent implements OnInit {
 
-  constructor() { }
+  menuState: string;
+
+  links = [
+    {
+      url: 'top',
+      name: 'Top',
+      state: 'active'
+    },
+    {
+      url: 'issue',
+      name: 'Issue',
+      state: 'inactive'
+    },
+    {
+      url: 'wiki',
+      name: 'Wiki',
+      state: 'inactive'
+    }
+  ];
+
+  constructor(
+    this.menuState = 'inactive'
+  ) { }
 
   ngOnInit() {
   }
 
+  onClick(i: number) {
+    this.links.forEach(
+      (element, index,arrary) => element.state = (i === index) ? 'active':'inactive'
+    );
+  }
 }
